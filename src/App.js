e017f89118a125eba68fa63cbc08035fdad2857f
Danna Gabriela Alvarado Perez <danna_3041240405@utd.edu.mx>
2026-04-26 21:03:27 -0600

    Actualización final

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import danna from './danna.jpeg';
 import './App.css';
 
 function App() {
+  const [usuario, setUsuario] = useState(null);
 
   function handleCredentialResponse(response) {
     console.log("Token JWT:", response.credential);
-    alert("Inicio de sesión con Google exitoso ");
+
+    setUsuario("Danna Alvarado");
   }
 
   React.useEffect(() => {
-    if (window.google) {
+    if (window.google && !usuario) {
       window.google.accounts.id.initialize({
         client_id: "275077458710-tjaj8gg3oscia4tq1meoqvs3loc0773q.apps.googleusercontent.com",
         callback: handleCredentialResponse,
@@ -26,8 +28,60 @@ function App() {
         }
       );
     }
-  }, []);
+  }, [usuario]);
 
+  function cerrarSesion() {
+    setUsuario(null);
+  }
+
+  // Pantalla después de iniciar sesión
+if (usuario) {
+  return (
+    <div className="App">
+      <header className="App-header">
+
+        {/* Aquí agregamos la misma imagen */}
+        <img src={danna} className="App-danna" alt="danna" />
+
+        <h1>Bienvenido(a), {usuario}</h1>
+
+        <p>EVALUACIÓN PARCIAL 3</p>
+
+        <a
+          className="App-link"
+          href={process.env.PUBLIC_URL + "/ERS.pdf"}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          DESCARGAR DOCUMENTO ERS DEL PROYECTO
+        </a>
+
+        <a
+          className="App-link"
+          href="https://alvaradoperezdannagabriela.atlassian.net/jira/software/projects/MUEB/boards/34"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          TABLERO JIRA PROYECTO SIBA
+        </a>
+
+        <button
+          onClick={cerrarSesion}
+          style={{
+            marginTop: "20px",
+            padding: "10px 20px",
+            fontSize: "16px",
+            cursor: "pointer"
+          }}
+        >
+          CERRAR SESIÓN PARCIAL 3
+        </button>
+
+      </header>
+    </div>
+  );
+}
+  // Pantalla inicial con login
   return (
     <div className="App">
       <header className="App-header">
@@ -65,7 +119,6 @@ function App() {
         <div style={{ marginTop: "30px" }}>
           <div id="googleSignInDiv"></div>
         </div>
-
       </header>
     </div>
   );
